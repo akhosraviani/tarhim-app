3fbaa627aaa27df6453952600fafaf6be83cad39
Farzad <frzdmansoori@yahoo.com>
2021-03-07 00:30:19 +0330

    Sample Request in MVVM for Fatemeh

--- app/src/main/java/ir/co/tarhim/ui/viewModels/HomeViewModel.kt
+++ app/src/main/java/ir/co/tarhim/ui/viewModels/HomeViewModel.kt
@@ -1,6 +1,22 @@
 package ir.co.tarhim.ui.viewModels
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import ir.co.tarhim.ui.repository.LoginRepositoy
+import okhttp3.ResponseBody
+import kotlin.math.log
 
 class HomeViewModel:ViewModel() {
+
+
+    private lateinit var loginRepositoy: LoginRepositoy
+    public lateinit var ldSignUp: LiveData<ResponseBody>
+    init {
+        loginRepositoy= LoginRepositoy()
+        ldSignUp=loginRepositoy.mldSignUp
+    }
+
+    fun  requestSignUp(number:String){
+        loginRepositoy.requestSignUp(number)
+    }
 }
